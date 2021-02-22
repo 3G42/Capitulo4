cac4ddd4cf6b638ec8bc6ebad55b0dacef9e5f49
Gabriel Gomes Garcia <gabriel.ggarcia@hotmail.com>
2021-02-22 17:24:04 -0300

    Update Capitulo 4

--- Capitulo4/Capitulo4/Capitulo4.cpp
+++ Capitulo4/Capitulo4/Capitulo4.cpp
@@ -10,15 +10,32 @@ int main()
 	// 4.4.1.1 - Comando if
 
 	
-	int a = 0;
-	int b = 0;
-	cout << "Por favor, digite dois inteiros \n";
-	cin >> a >> b;
-	if (a < b)
-		// 1ª alternativa(usada se a condição eh verdadeira):
-		cout << "max(" << a << "," << b << ") é" << b <<"\n";
+	//int a = 0;
+	//int b = 0;
+	//cout << "Por favor, digite dois inteiros \n";
+	//cin >> a >> b;
+	//if (a < b)
+	//	// 1ª alternativa(usada se a condição eh verdadeira):
+	//	cout << "max(" << a << "," << b << ") é" << b <<"\n";
+	//else
+	//	// 2ª alternativa ( usada se a condição eh falsa): 
+	//	cout << "max(" << a << "," << b << ") é " << a << "\n";
+	//
+	// Conversor de polegadas para centimetros
+
+	const double cm_por_pol = 2.54;
+	double comprimento = 1;
+	char unidade = ' ';
+	cout << " Por favor, informe o comprimento seguido pela unidade (c ou p):\n";
+	cin >> comprimento >> unidade;
+
+	if (unidade == 'p')
+		cout << comprimento << "pol == " << cm_por_pol * comprimento << 
+		"cm\n";
+	else if (unidade == 'c')
+		cout << comprimento << "cm == " << comprimento / cm_por_pol << 
+		"pol\n";
 	else
-		// 2ª alternativa ( usada se a condição eh falsa): 
-		cout << "max(" << a << "," << b << ") é " << a << "\n";
-	
+		cout << "Desculpe não reconheço a unidade indicada por " << 
+		 unidade << "\n";
 }
